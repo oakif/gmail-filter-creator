b8387066c63b5f9e8039b34bd6add856e5af99b0
Oion Akif <48932412+oakif@users.noreply.github.com>
2026-01-09 20:52:23 -0500

    set up project structure and dependencies

---
+++ gmail_filter_converter/__init__.py
@@ -0,0 +1,6 @@
+"""Gmail Filter XML ↔ YAML Converter
+
+Bidirectional converter between Gmail filter export format (XML) and YAML.
+"""
+
+__version__ = '0.1.0'
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Tests for gmail-filter-converter"""
